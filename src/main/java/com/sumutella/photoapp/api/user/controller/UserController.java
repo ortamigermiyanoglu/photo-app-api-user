6acb26aff8b872a33bfa529b876e1b9ee860c1c3
sumut <fatih.ek96@hotmail.com>
2020-07-14 00:33:18 +0300

    inital commit

---
+++ src/main/java/com/sumutella/photoapp/api/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.sumutella.photoapp.api.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/status")
+    public String status(){
+        return "working mien";
+    }
+}
---
+++ src/test/java/com/sumutella/photoapp/api/user/PhotoAppApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumutella.photoapp.api.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
